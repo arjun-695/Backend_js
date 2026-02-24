8b4e6abacc4d974c6886fcc17c04bb1cb45b5f5d
Arjun Tandon <tandonarjun695@gmail.com>
2026-02-24 21:48:22 +0530

    feat: Implement playlist routes with JWT verification and object ID validation

---
+++ src/routes/playlist.routes.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import {
+    createPlaylist,
+  getUserPlaylists,
+  getPlaylistById,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  updatePlaylist,
+} from "../controllers/playlist.controller.js"
+
+import { verifyJWT} from "../middlewares/auth.middleware.js"
+import checkValidObjectId from '../middlewares/validateObjectId.middleware.js';
+
+const router = Router();
+
+router.use(verifyJWT) // apply verifyJWT to all the routes in this file
+
+router.route("/").post(createPlaylist)
+router.route("/user/:userId").get(checkValidObjectId(["userId"]),getUserPlaylists)
+
+router
+        .route("/:playlistId")
+        .get(checkValidObjectId(["playlistId"]), getPlaylistById)
+        .patch(checkValidObjectId(["playlistId"]), updatePlaylist)
+        .delete(checkValidObjectId(["playlistId"]), deletePlaylist)
+        
+router.route("/add/:videoId/:playlistId").patch(checkValidObjectId(["videoId", "playlistId"]), addVideoToPlaylist)
+
+router.route("/remove/:videoId/:playlistId").patch(checkValidObjectId(["videoId", "playlistId"]), removeVideoFromPlaylist);
+
+export default router
