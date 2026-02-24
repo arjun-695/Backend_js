f4659e5206a5ab7f8ca752b6320ed627bb621331
Arjun Tandon <tandonarjun695@gmail.com>
2026-02-24 22:00:33 +0530

    feat: Add middleware to validate ObjectId format in request parameters

---
+++ src/middlewares/validateObjectId.middleware.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { ApiError } from "../utils/apiError";
+
+export const checkValidObjectId = (paramNamesArray) => {
+
+    return (req,res, next) => {
+        for( const paraName of paramNamesArray){
+            const id = req.params[paraName];
+
+            if( id && !mongoose .isValidObjectId(id)){
+                return next(new ApiError(400, `Invalid ${paraName} format `));
+            }
+        }
+        next(); //ID's are okay
+    }
+}
