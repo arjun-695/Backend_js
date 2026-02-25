a2377582a304707185b920569858b1492eeaa574
Arjun Tandon <tandonarjun695@gmail.com>
2026-02-25 07:52:30 +0530

    feat: Add video, comment, playlist, subscription, and like routes to app.js

--- src/app.js
+++ src/app.js
@@ -21,10 +21,21 @@ app.use(cookieParser())
 
 //routes import 
 import userRouter from './routes/user.routes.js'
+import videoRouter from './routes/video.routes.js'
+import commentRouter from './routes/comment.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
+import likeRouter from './routes/like.routes.js'
 
 
 // routes declaration
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/playlists", playlistRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/likes", likeRouter)
+//Mechanism:User requests `//localhost:8000/api/v1/users` then `app.use()` transfers the request to `userRouter`. Inside `userRouter`,it routes to `/register` and calls the controller `registerUser` from `user.controller.js` . 
 export { app }
 
 
